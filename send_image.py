7f3c840f8a53050dd7c7dd150451de2f3f60cd41
jezman <jez.studio@gmail.com>
2017-03-02 11:39:28 +0300

    edit import

--- send_image.py
+++ send_image.py
@@ -3,43 +3,43 @@
 import os
 import requests
 import shutil
+from datetime import datetime
 from sys import argv
 from time import strftime
 
 date = strftime('%d-%m-%Y')
-dir = '/path_to_dir/'
-hours = range(7, 17)
+dir = '/video/'
+hours = range(7, 19)
 hour = int(strftime('%H'))
 days = ('6', '7')
 now = datetime.now()
 weekday = str(now.isoweekday())
 
 
-def sends(path_to_file):
-    url = "https://api.telegram.org/bot"BOT_ID"/sendPhoto"
-    files = {'photo': open(path_to_file, 'rb')}
-    data = {'chat_id': "CHAT_ID"}
-    r = requests.post(url, files=files, data=data)
-    # print(r.status_code, r.reason, r.content)
+def sends(path):
+    url = "https://api.telegram.org/botYour_bot_id/sendPhoto"
+    files = {'photo': open(path, 'rb')}
+    data = {'chat_id': "your_chat_id"}
+    requests.post(url, files=files, data=data)
 
 
 def sensor():
-    fl = open('path_to_rid')
+    fl = open('rid.txt')
     motion = fl.read()
     if '1' in motion:
         return True
 
 
-def moveFiles(extention):
-    files = os.listdir(dir)
-    to_move = filter(lambda x: x.endswith('.{ext}'.format(ext=ext)), files)
+def moveFiles(ext):
+    fls = os.listdir(dir)
+    to_move = filter(lambda x: x.endswith('.{ext}'.format(ext=ext)), fls)
     path = r'{dir}{date}'.format(dir=dir, date=date)
 
     if not os.path.exists(path):
         os.makedirs(path)
 
     for obj in to_move:
-        shutil.move('{dir}{files}'.format(files=obj, dir=dir),
+        shutil.move('{dir}{fls}'.format(fls=obj, dir=dir),
                     '{dir}{date}'.format(dir=dir, date=date))
 
 
